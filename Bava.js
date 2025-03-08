354cace1aaeb484d958ae0c4942572ada1574a7f
SOKCHIN123 <118110623+SOKCHIN123@users.noreply.github.com>
2025-03-08 20:35:20 +0700

    Add files via upload

---
+++ Bava.js
@@ -0,0 +1,31 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".exchange-btn button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            // Remove "active" class from all buttons
+            buttons.forEach(btn => btn.classList.remove("active"));
+            
+            // Add "active" class to the clicked button
+            this.classList.add("active");
+        });
+    });
+});
+
+
+
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+
+
